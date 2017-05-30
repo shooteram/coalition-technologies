dadbdb9786442c22af0eebc5e956c90be943c0e0
shooteram <me@shooteram.fr>
2017-05-30 23:59:02 +0200

    View updated

--- resources/assets/js/components/Home.vue
+++ resources/assets/js/components/Home.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <div class="col-md-8 col-md-offset-2">
+        <div class="col-md-8 col-md-offset-2" v-show="! error.status">
             <div class="panel panel-default">
                 <div class="panel-heading">Add a new product</div>
 
@@ -44,38 +44,49 @@
                             </div>
                         </div>
                     </form>
+
+                    <div v-show="empty" class="alert alert-info">
+                        <strong>There is no product yet. </strong> You can create one right now using the form which is right above this message.
+                    </div>
                 </div>
+
+                <table class="table table-hover" v-show="! empty">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Product name</th>
+                            <th>Quantity in stock</th>
+                            <th>Price per item</th>
+                            <th>Date of creation</th>
+                            <th>Total</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        <tr v-for="product in products">
+                            <td>{{ product.id }}</td>
+                            <td>{{ product.name }}</td>
+                            <td>{{ product.quantity }}</td>
+                            <td>${{ product.price }}</td>
+                            <td>{{ product.created_at }}</td>
+                            <td>${{ product.quantity * product.price }}</td>
+                        </tr>
+
+                        <tr>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>Total</td>
+                            <td>${{ total }}</td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
 
-        <table class="table table-hover" v-show="! empty">
-            <thead>
-                <tr>
-                    <th>#</th>
-                    <th>Product name</th>
-                    <th>Quantity in stock</th>
-                    <th>Price per item</th>
-                    <th>Date of creation</th>
-                </tr>
-            </thead>
-
-            <tbody>
-                <tr v-for="product in products">
-                    <td>{{ product.id }}</td>
-                    <td>{{ product.name }}</td>
-                    <td>{{ product.quantity }}</td>
-                    <td>{{ product.price }}</td>
-                    <td>{{ product.created_at }}</td>
-                </tr>
-            </tbody>
-        </table>
-
         <div v-show="error.status" class="alert alert-warning">
-            <strong>{{ error.title }}! </strong> <span v-html="error.message"></span>
-        </div>
-
-        <div v-show="empty" class="alert alert-info">
-            <strong>There is no product yet. </strong> You can create one right now using the form which is right above this message.
+            <strong>{{ error.title }}. </strong> <span v-html="error.message"></span>
         </div>
     </div>
 </template>
@@ -95,34 +106,50 @@ export default {
         products: [],
         product_name: '',
         product_quantity: '',
-        product_price: ''
+        product_price: '',
+        total: 0
     }),
     mounted() {
-        var that = this;
-
-        api.get('products')
-            .then( function(res) {
-                that.products = res.data
-                if (res.data.length > 0) {
-                    that.empty = false
-                } else {
-                    that.empty = true
-                }
-            })
-            .catch( function(err) {
-                that.error.status = true
-
-                if (err.response) {
-                    that.error.title = 'Error ' + err.response.status
-                    that.error.message = 'Couldn\'t load the client variables.'
-                } else {
-                    that.error.title = 'Error ' + err.response.status
-                }
-            });
+        this.show()
     },
     methods: {
+        show() {
+            var that = this
+
+            api.get('products')
+                .then( function(res) {
+                    if (res.data.length > 0) {
+                        that.products = res.data
+                        res.data.map( function(product) {
+                            that.total = that.total + (product.quantity * product.price)
+                        })
+                        that.empty = false
+                    } else {
+                        that.empty = true
+                    }
+                })
+                .catch( function(err) {
+                    that.error.status = true
+
+                    if (err.response) {
+                        that.error.title = 'Error ' + err.response.status
+                        that.error.message = 'Couldn\'t load the products.'
+                    } else {
+                        that.error.title = 'Error ' + err.response.status
+                    }
+                })
+        },
         create() {
-            api.post('products', { 'name': this.product_name, 'quantity': this.product_quantity, 'price': this.product_price });
+            api.post('products', { 'name': this.product_name, 'quantity': this.product_quantity, 'price': this.product_price })
+            this.show()
+            this.product_name = ''
+            this.product_quantity = ''
+            this.product_price = ''
+        },
+        total() {
+            this.products.map( function(p) {
+                // this.total += p.
+            })
         }
     }
 }
