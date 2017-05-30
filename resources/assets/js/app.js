3c1f6bf5b3fc38ac1d25446dbb7faa759cbf62a1
shooteram <me@shooteram.fr>
2017-05-30 22:24:20 +0200

    Mount point for the SPA

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,22 +1,17 @@
+import Vue from 'vue'
+import App from './components/App.vue'
+import router from './router'
+import './bootstrap'
 
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
+router.beforeEach((to, from, next) => {
+  document.title = to.meta.title + ' - ' + window.Global.app_name
+  next()
+})
 
-require('./bootstrap');
-
-window.Vue = require('vue');
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
-Vue.component('example', require('./components/Example.vue'));
-
-const app = new Vue({
-    el: '#app'
-});
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: { App }
+})
