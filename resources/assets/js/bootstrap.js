d1d8b63ea639f6c9d5680d6282b64a0528b3c9ff
shooteram <me@shooteram.fr>
2017-05-30 22:44:46 +0200

    Exported the 'api' constant for axios

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -37,6 +37,10 @@ if (token) {
     console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
 }
 
+export const api = axios.create({
+  baseURL: window.Global.app_domain + `/api/`
+})
+
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
